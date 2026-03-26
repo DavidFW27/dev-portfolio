536c0016b68a67ffdd3d7e2a5c4a3215a1112aa6
David Fonseca Roman <davifonsecar27@gmail.com>
2026-03-26 13:19:56 -0600

    Add delete task feature

---
+++ To-do-app/script.js
@@ -0,0 +1,15 @@
+function addTask(){
+
+let input = document.getElementById("taskInput")
+
+let taskText = input.value
+
+let li = document.createElement("li")
+
+li.textContent = taskText
+
+document.getElementById("taskList").appendChild(li)
+
+input.value = ""
+
+}
